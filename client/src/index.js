6213e51f30f40337a5317a1a836f3f1e7873fb54
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-13 11:11:58 -0400

    turned off cors and added proxy in client side

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import { createContext } from 'react'
 import './index.css';
 import App from './App';
 import actionCable from 'actioncable'
-import reportWebVitals from './reportWebVitals';
 
 const CableApp = {}
 CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable') // change to whatever port your server uses
@@ -19,8 +18,3 @@ root.render(
     </ActionCableContext.Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
