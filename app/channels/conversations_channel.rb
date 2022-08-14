53625329fadd11d2156fcea83a335651433700b8
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-14 16:05:56 -0400

    added name input and channel selections

--- app/channels/conversations_channel.rb
+++ app/channels/conversations_channel.rb
@@ -2,15 +2,19 @@ class ConversationsChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     stop_all_streams
-    stream_from "conversations_channel"
+    @username = params[:username]
+    @channel = params[:channel_id]
+    stream_from @channel
+    ActionCable.server.broadcast @channel, "#{@username} joined channel ##{@channel}"
   end
   
   def receive(data)
-    ActionCable.server.broadcast "conversations_channel", data
+    ActionCable.server.broadcast @channel, "#{@username}: #{data["message"]}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    ActionCable.server.broadcast @channel, "#{@username} left channel ##{@channel}"
     stop_all_streams
   end
 end
--- client/src/MessageList.js
+++ client/src/MessageList.js
@@ -2,35 +2,68 @@ import { useContext, useEffect, useState } from 'react';
 import { ActionCableContext } from '.';
 
 export default function MessageList() {
+  const [username, setUsername] = useState("")
   const [channel, setChannel] = useState("")
+  const [channelIdInput, setChannelIdInput] = useState(1)
+  const [channelId, setChannelId] = useState("")
+  const [message, setMessage] = useState("")
+
   const cable = useContext(ActionCableContext)
   
   useEffect(() => {
-    const channel = cable.subscriptions.create({
-      channel: "ConversationsChannel"
-    }, {
-      received: (data) => {
-        console.log(data)
+    if (channelId) {
+      const channel = cable.subscriptions.create({
+        channel: "ConversationsChannel",
+        channel_id: channelId,
+        username: username
+      }, {
+        received: (data) => {
+          console.log(data)
+        }
+      })
+  
+      setChannel(channel)
+
+      return () => {
+        channel.unsubscribe()
       }
-    })
-
-    // const channel = cable.subscriptions.create("conversations_channel")
-    // const channel = cable.subscriptions.create("ConversationsChannel")
-
-    setChannel(channel)
-
-    return () => {
-      channel.unsubscribe()
     }
-  },[])
+  },[channelId])
+
+  function joinChannel(e) {
+    if (username) {
+      setChannelId(channelIdInput)
+    } else {
+      alert("please enter your name")
+    }
+  }
 
   function sendData() {
-    channel.send({ message: "This is a cool chat app." })
+    if (channel) {
+      if (message) {
+        channel.send({ message: message })
+        setMessage("")
+      } else {
+        alert("please enter your message")
+      }
+    } else {
+      alert("please join a channel first")
+    }
   }
 
   return (
     <div>
-      <button onClick={sendData}>Send data</button>
+      <div>
+        <input type="text" maxLength="20" placeholder='please enter your name' value={username} onChange={e => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <input type="number" min="1" value={channelIdInput} onChange={e => setChannelIdInput(e.target.value)} />
+        <button onClick={joinChannel}>Join Channel</button>
+      </div>
+      <div>
+        <input type="text" value={message} onChange={e => setMessage(e.target.value)} />
+        <button onClick={sendData}>Send data</button>
+      </div>
     </div>
   );
 }
